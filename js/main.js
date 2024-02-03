e0f71b4a3f134d24780d86af1e293a7acaf92b3f
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-03 11:19:08 -0300

    Adicionei o sistema de busca(1).

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// const endpoint = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=ab0c77a&s=superman')
+
+const btn = document.querySelector('#btn')
+const search = document.querySelector('#search')
+const container = document.querySelector('#container')
+
+const get_movie = async (id) =>{
+    try{
+        const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=ab0c77a&s=${id}`)
+        const data = await response.json()
+
+        container.innerHTML = ''
+
+        data.Search.forEach(element => {
+            const card = document.createElement('div')
+            const img = document.createElement('img')
+            img.src = element.Poster
+
+            card.appendChild(img)
+            container.appendChild(card)
+            
+            
+        }); 
+    }catch(err){
+        console.log(err+ ' '+'Ocorreu algum erro.')
+    }
+}
+
+
+btn.addEventListener('click',()=>{
+    const name = search.value
+    if (name===''){
+        alert('Digite um nome de um filme.')
+    } else {
+        get_movie(name)
+    }
+    
+})
+document.addEventListener('keyup',()=>{
+    const name = search.value
+    if (event.keyCode === 13){
+        if (name===''){
+            alert('Digite um nome de um filme.')
+        } else {
+            get_movie(name)
+        }
+    }
+})
